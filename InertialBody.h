5014bb810791ef233c83401decc30e375a2544fb
jujuroa <jujuroa@gmail.com>
2025-12-17 14:15:58 +0900

    init project

---
+++ InertialBody.h
@@ -0,0 +1,74 @@
+/*
+ * InertialBody.h
+ *
+ *  Created on: Dec 16, 2025
+ *      Author: youngh
+ */
+
+#ifndef INERTIALBODY_H_
+#define INERTIALBODY_H_
+
+#include <cmath>
+
+#include "logger/Logger.h"
+
+namespace easing {
+
+class InertialBody {
+ public:
+  struct InertialState {
+    double target;
+    double position;
+    double velocity;
+    double accelation;
+
+    InertialState(double _target = .0, double _position = .0,
+                  double _velocity = .0, double _accelation = .0)
+        : target(_target),
+          position(_position),
+          velocity(_velocity),
+          accelation(_accelation) {}
+    void reset() {
+      target = .0;
+      position = .0;
+      velocity = .0;
+      accelation = .0;
+    }
+    void step(double _accelation){
+    	accelation = _accelation;
+    	velocity += accelation;
+    	position += velocity;
+    }
+    double getDistance() { return target - position; }
+  };
+
+  InertialBody(double _elasticity = .5, double _friction = .5,
+               double _mass = .5, double _distanceExponent = 0);
+
+  virtual ~InertialBody();
+
+  void resetMovement();
+  InertialState step();
+
+  const InertialState& getCurrentStatus() const;
+  double getDistanceExponent() const;
+  void setDistanceExponent(double distanceExponent);
+  double getElasticity() const;
+  void setElasticity(double elasticity);
+  double getFriction() const;
+  void setFriction(double friction);
+  double getMass() const;
+  void setMass(double mass);
+
+ private:
+  InertialState currentStatus;
+
+  double elasticity;
+  double friction;
+  double mass;
+  double distanceExponent;
+};
+
+} /* namespace easing */
+
+#endif /* INERTIALBODY_H_ */
---
+++ build/config.h
@@ -0,0 +1,2 @@
+#define InertialBody_VERSION_MAJOR 0
+#define InertialBody_VERSION_MINOR 1
